32fc173b4e81bc2e6e09e3bb2a2a3c596bb86c34
NikolaMrkic <nikola.mrkic93@gmail.com>
2020-09-04 19:15:55 +0200

    adding router, not found page and global imports

---
+++ src/global.js
@@ -0,0 +1,17 @@
+// imports from react
+import React, { useState, Component } from "react";
+
+// imports from react router
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  useLocation,
+} from "react-router-dom";
+
+// imports from semantic-ui-react
+import { Segment } from "semantic-ui-react";
+
+export { React, useState, Component, Segment, Router, Switch, Route };
